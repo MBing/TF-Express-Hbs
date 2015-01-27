311962552e09e501b0b97f6bc0666185bf6701a6
Martin <bing.martin.photography@gmail.com>
2015-01-27 08:44:37 +0100

    Refactor code and add fallback for default value

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express'),
-	port = 9000;
-
+    expressHbs = require('express-handlebars');
+    
+var port = process.env.PORT || 9000;
 var app = express();
-var expressHbs = require('express-handlebars');
 
 app.engine('hbs', expressHbs({extname: 'hbs', defaultLayout: 'main.hbs'}));
 app.set('view engine', 'hbs');
 // When you put the views in another folder than 'views'
 // app.set('views', __dirname + '/other_folder')
+app.get('/', function (req, res) {
+	res.render('greeting', { title: 'Simple Greeting App', name: 'world'
+	});
+})
 app.get('/:name', function (req, res) {
 	res.render('greeting', { title: 'Simple Greeting App', name: req.params.name 
 	});
